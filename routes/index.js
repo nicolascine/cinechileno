91ef8d8339c3659d063254d27fd736b34c33baaa
nicolas silva <sovietfrance@gmail.com>
2015-01-05 23:31:06 -0300

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'cineApp' });
+  res.render('index', { title: 'CineChileno' });
 });
 
 module.exports = router;
